91d9f7c85bacbb853118046f66b2c721ccf7fdb6
mowafy001 <wafy123445@gmail.com>
2023-04-24 01:50:07 +0200

    feat: verify JWT and attach the payload to the request

--- src/middlewares/specific.ts
+++ src/middlewares/specific.ts
@@ -1,10 +1,26 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
+import { HttpError } from './error-handling';
+import { verifyJWT } from '../services/auth/jwt';
 
-export const authMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.log("authMiddleware");
-  next();
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Get the authorization header
+        const authHeader = req.headers.authorization;
+        if (!authHeader) throw new Error('Authorization header is required');
+
+        // Get the token
+        const token = authHeader.split(' ')[1];
+        if (!token) throw new Error('Access token required');
+
+        // Verify the token
+        const decoded = verifyJWT(token);
+
+        // Attach the user to the request
+        // @ts-ignore
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+        next(new HttpError(401, error.message));
+    }
 };
